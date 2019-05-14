8666967ebf2d62a0c3341e9952f4b3d55e1e7b3d
zhou.xiaofeng <zhou.xiaofeng111@iwhalecloud.com>
2019-05-14 14:37:39 +0800

    add server

--- spring-cloud-learning/config/config-client/src/main/java/com/springcloud/windmill/configclient/ConfigClientApplication.java
+++ spring-cloud-learning/config/config-client/src/main/java/com/springcloud/windmill/configclient/ConfigClientApplication.java
@@ -3,17 +3,11 @@ package com.springcloud.windmill.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableEurekaClient
-@EnableDiscoveryClient
-@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -22,7 +16,6 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
-
     @RequestMapping(value = "/hai")
     public String hi(){
         return foo;
