df7223d42e7f40daa1de96075e930fc967cc3232
alvin <alvinzxf@163.com>
2019-05-15 10:55:35 +0800

    multi-server

---
+++ spring-cloud-learning/windmill-lesson10/eureka-client/src/main/java/com/springcloud/windmill/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,31 @@
+package com.springcloud.windmill.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author windmill
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home(@RequestParam(value = "name", defaultValue = "windmill") String name) {
+        return "hello " + name + " ,i am from port:" + port;
+    }
+
+}
