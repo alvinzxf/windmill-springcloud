0652fa5123b85431110b2e3fe09fac4ebeaae070
alvin <alvinzxf@163.com>
2019-05-17 17:10:19 +0800

    add gateway

---
+++ spring-cloud-learning/gateway-limit/src/main/java/com/springcloud/windmill/gatewaylimit/GatewayLimitApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.windmill.gatewaylimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayLimitApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-learning/gateway-limit/src/main/java/com/springcloud/windmill/gatewaylimit/HostAddrKeyResolver.java
@@ -0,0 +1,17 @@
+package com.springcloud.windmill.gatewaylimit;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author windmill
+ * @description
+ * @date 2019-05-17
+ */
+public class HostAddrKeyResolver implements KeyResolver {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+}
